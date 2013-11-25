acb9ed4e139086b99d0adec21091bf0d30d671d1
lplit <lplit@wp.pl>
2013-11-25 14:10:57 +0100

    -

--- events.ml
+++ events.ml
@@ -4,46 +4,25 @@ let keys = [ (Sdlkey.KEY_UP, false) ;
 	     (Sdlkey.KEY_DOWN, false) ;  
 	     (Sdlkey.KEY_RIGHT, false) ; 
 	     (Sdlkey.KEY_LEFT, false) ; 
+	     (Sdlkey.KEY_SPACE, false) ; 
 	     (Sdlkey.KEY_ESCAPE, false) ] ;;
 
 let get_keys = 
-  let rec loop acc = 
-    match keys with 
-    | [] -> acc
-    | ((Sdlkey.KEY_DOWN, true) as h)::t
-    | ((Sdlkey.KEY_RIGHT, true) as h)::t
-    | ((Sdlkey.KEY_LEFT, true) as h)::t
-    | ((Sdlkey.KEY_ESCAPE, true) as h) -> loop h::acc
-    | _::t -> acc t ;
-  in 
-  loop []
+  filter -> map
 ;;
 
-(*
-let update = 
-  (* treat two events and ignore rest *)
+let update (event:Sdlevent.event) =
+  match event.ke_state with
+  | PRESSED ->
+  | RELEASED ->
 ;;
-
-let updates = 
   
+let updates (evlist:Sdlevent.event list) = 
+  List.iter update evlist
 ;;
 
-*)
 (*
-val get_keys : unit -> Sdlkey.t list
-val update : Sdlevent.event -> unit
-val updates : Sdlevent.event list -> unit
-*)
-   
-
-(* http://ocamlsdl.sourceforge.net/doc/html/Sdlkey.html
-
-variable globale dans events let keys = (liste de couples definie a la main (touche sdlkey, bool ref))
-Deux evenments only - pressed and unpressed 
-
-ocamlfind ocamlc -package sdl -c events.ml
-ocamlfind compiler -package list_of_packages -c file_to_compile.ml
-
+http://ocamlsdl.sourceforge.net/doc/html/Sdlkey.html
 
 type keyboard_event = {
   ke_which : int;(*The keyboard device index*)
@@ -59,6 +38,4 @@ match x.keysym with
 Sdlkey.KEY_ESCAPE
 | Sdlkey.KEY_q -> false
 | _ -> true
-
-
 *)
