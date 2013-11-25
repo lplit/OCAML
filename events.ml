68d9233d460f3409e5dfd7aa9ec8852e24db0085
lplit <lplit@wp.pl>
2013-11-25 16:14:44 +0100

    update, updates

--- events.ml
+++ events.ml
@@ -1,41 +1,36 @@
 type key = (Sdlkey.t, bool ref) list;;
 
-let keys = [ (Sdlkey.KEY_UP, false) ; 
-	     (Sdlkey.KEY_DOWN, false) ;  
-	     (Sdlkey.KEY_RIGHT, false) ; 
-	     (Sdlkey.KEY_LEFT, false) ; 
-	     (Sdlkey.KEY_SPACE, false) ; 
-	     (Sdlkey.KEY_ESCAPE, false) ] ;;
+(* True = key pressed, false otherwise *)
+let keys = [ (Sdlkey.KEY_UP, ref false) ; 
+	     (Sdlkey.KEY_DOWN, ref false) ;  
+	     (Sdlkey.KEY_RIGHT, ref false) ; 
+	     (Sdlkey.KEY_LEFT, ref false) ; 
+	     (Sdlkey.KEY_SPACE, ref false) ; 
+	     (Sdlkey.KEY_ESCAPE, ref false) ] ;;
 
+
+(* List of keys pressed *)
 let get_keys = 
-  filter -> map
+  let tmp = List.filter ((a,b) -> !b=true) keys in 
+  List.map fst tmp
 ;;
 
-let update (event:Sdlevent.event) =
-  match event.ke_state with
-  | PRESSED ->
-  | RELEASED ->
+
+(* Updates keypress *)
+let update e =
+  try
+    match e with
+    | KEYDOWN { keysym = sym } ->
+      (List.assoc e keys) := true
+    | KEYUP { keysym = sym } ->
+      (List.assoc e keys) := false 
+    | _ ->
+      ()
+  with Not_found ->
+    ()
 ;;
-  
+
+      
 let updates (evlist:Sdlevent.event list) = 
   List.iter update evlist
 ;;
-
-(*
-http://ocamlsdl.sourceforge.net/doc/html/Sdlkey.html
-
-type keyboard_event = {
-  ke_which : int;(*The keyboard device index*)
-  ke_state : switch_state;(*PRESSED or RELEASED*)
-  keysym : Sdlkey.t;(*SDL virtual keysym*)
-  keymod : Sdlkey.mod_state;(*current key modifiers*)
-  keycode : char;(*translated character*)
-  unicode : int;
-}
-
-let onkeydown x =
-match x.keysym with
-Sdlkey.KEY_ESCAPE
-| Sdlkey.KEY_q -> false
-| _ -> true
-*)
