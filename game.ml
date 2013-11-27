521bdbadd51d04ed2385365854ebee06434ffadb
lplit <lplit@wp.pl>
2013-11-27 16:57:26 +0100

    -

--- game.ml
+++ game.ml
@@ -1,31 +1,40 @@
 open Graphics
-
-module G = (val (init ()) : G)
+open Geometry
+module G = (val (init ()) : G) 
 
 type state = { 
   p : BoundingBox.Circle.t } 
 
 (* Global variables *) 
-let player = state ref;;
-let speed = state.p.v ;;
-let mv_left = Vector.create -5 0;;
-let mv_right = Vector.create 5 0;; 
-let mv_down = Vector.create 0 5;;
-let mv_up = Vector.create 0 -5;;
+let mv_left:Vector.t = Vector.create -5 0 ;
+let mv_right = Vector.create 5 0 ;
+let mv_down = Vector.create 0 5 ;
+let mv_up = Vector.create 0 -5 ;
 
 (* Functions *)
-let update st:state = st;;
 
-let display st=
-    G.flip ();
-;;
+(* gere les if key pressed = move key then call move_player, 
+   ignore rest, escape quits, space shoots, s shows score *)
+let update (key:Sdlkey.t) (state:state) : state =
+  match key with 
+  | Sdlkey.KEY_UP
+  | Sdlkey.KEY_DOWN
+  | Sdlkey.KEY_RIGHT
+  | Sdlkey.KEY_LEFT -> move_player key state
+  | Sdlkey.KEY_ESCAPE -> G.quit() 
+  | Sdlkey.KEY_SPACE -> state 
 
-let play =
+
+let display () =
+    G.flip()
+
+
+let play () =
   while true do 
-    update player;
-    display player;
+    updates()
+    display()
   done
-;;
+
 
 (* Key + state(player) -> player with modfied position *)
 let move_player (key:Sdlkey.t) (state:state) : state = 
@@ -36,24 +45,15 @@ let move_player (key:Sdlkey.t) (state:state) : state =
   | Sdlkey.KEY_RIGHT -> Circle.move mv_right state.p
   | Sdlkey.KEY_LEFT -> Circle.move mv_left state.p
   | _ -> state
-;;
 
-(* gere les if key pressed = move key then call move_player, 
-   ignore rest, escape quits, space shoots, s shows score *)
-let update (key:Sdlkey.t) (state:state) : state =
-  match key with 
-  | Sdlkey.KEY_UP
-  | Sdlkey.KEY_DOWN
-  | Sdlkey.KEY_RIGHT
-  | Sdlkey.KEY_LEFT -> move_player key state
-  | Sdlkey.KEY_ESCAPE -> G.quit () 
-  | Sdlkey.KEY_SPACE ->
-;;
+
 
 (* gets all the pressed keys (events) , calls update on all the keys *)
 let updates (state:state) : state = 
-  Events.get_keys () 
-;;
+  List.iter update Events.get_keys () 
+
+
+
 (*
 module type G =
   sig
@@ -75,3 +75,4 @@ module type G =
 val init : unit -> (module G)
 val quit : unit -> unit
 *)
+
