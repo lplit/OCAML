c5dca13b069292b02134242903d130a9a890062a
lplit <lplit@wp.pl>
2013-11-27 14:44:50 +0100

    Created move_player, modified update

--- game.ml
+++ game.ml
@@ -2,28 +2,59 @@ open Graphics
 
 module G = (val (init ()) : G)
 
-type state = { p : BoundingBox.Circle.t }
-    
-let update x = x;;
+type state = { 
+  p : BoundingBox.Circle.t } 
 
-let display =
-    G.flip;
+(* Global variables *) 
+let player = state ref;;
+let speed = state.p.v ;;
+let mv_left = Vector.create -5 0;;
+let mv_right = Vector.create 5 0;; 
+let mv_down = Vector.create 0 5;;
+let mv_up = Vector.create 0 -5;;
+
+(* Functions *)
+let update st:state = st;;
+
+let display st=
+    G.flip ();
 ;;
 
 let play =
   while true do 
-    begin 
-      update ;
-      display ;
-      G.flip;
-    end 
+    update player;
+    display player;
   done
 ;;
 
+(* Key + state(player) -> player with modfied position *)
+let move_player (key:Sdlkey.t) (state:state) : state = 
+  match key with 
+  (* Move v:vector c:circle *)
+  | Sdlkey.KEY_UP -> Circle.move mv_up state.p
+  | Sdlkey.KEY_DOWN -> Circle.move mv_down state.p
+  | Sdlkey.KEY_RIGHT -> Circle.move mv_right state.p
+  | Sdlkey.KEY_LEFT -> Circle.move mv_left state.p
+  | _ -> state
+;;
 
-		     
-(* 
+(* gere les if key pressed = move key then call move_player, 
+   ignore rest, escape quits, space shoots, s shows score *)
+let update (key:Sdlkey.t) (state:state) : state =
+  match key with 
+  | Sdlkey.KEY_UP
+  | Sdlkey.KEY_DOWN
+  | Sdlkey.KEY_RIGHT
+  | Sdlkey.KEY_LEFT -> move_player key state
+  | Sdlkey.KEY_ESCAPE -> G.quit () 
+  | Sdlkey.KEY_SPACE ->
+;;
 
+(* gets all the pressed keys (events) , calls update on all the keys *)
+let updates (state:state) : state = 
+  Events.get_keys () 
+;;
+(*
 module type G =
   sig
     val w : float
@@ -43,5 +74,4 @@ module type G =
   end
 val init : unit -> (module G)
 val quit : unit -> unit
-
 *)
